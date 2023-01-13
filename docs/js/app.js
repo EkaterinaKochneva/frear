c154332008efdf5500de696f27cffb91c7151e16
EkaterinaKochneva <kochnevaes@gmail.com>
2023-01-13 18:02:09 +0300

    continue main page

--- docs/js/app.js
+++ docs/js/app.js
@@ -27,9 +27,9 @@ $(document).ready(function(){
 		loop: true,
 		slidesPerView: 1,
 		draggable: true,
-		// autoplay: {
-		// 	delay: 5000,
-		//   },
+		autoplay: {
+			delay: 5000,
+		  },
 
         navigation: {
         nextEl: '.top-banner__button-next',
@@ -40,6 +40,39 @@ $(document).ready(function(){
 			el: '.top-banner__pagination',
 			clickable: true,
 		  },
+	});
+
+    // Cлайдер выполненных работ
+	const swiperCompletedWork = new Swiper('.completed-work__slider', {
+		loop: true,
+		slidesPerView: 1,
+		draggable: true,
+		autoplay: {
+			delay: 5000,
+		  },
+
+        navigation: {
+        nextEl: '.completed-work__button-next',
+        prevEl: '.completed-work__button-prev',
+        },
+
+		pagination: {
+			el: '.completed-work__pagination',
+			clickable: true,
+		  },
+	});
+
+    // Аккардион 
+	$('.accordion-block__more').click(function(){         
+
+		if(!$(this).hasClass('active')){	
+            $(this).addClass('active').text('Скрыть');
+            $(this).parents('.accordion-block__item').find('.accordion-block__desc').slideDown(); 
+				
+		} else {	
+			$(this).removeClass('active').text('Подробнее');
+			$(this).parents('.accordion-block__item').find('.accordion-block__desc').slideUp(); 
+		}
 	});
 
 })
