5103324422307ae7f7b304d0faa2b262d1c7a5d3
EkaterinaKochneva <kochnevaes@gmail.com>
2023-01-19 18:35:48 +0300

    continue category

--- docs/js/shop.js
+++ docs/js/shop.js
@@ -8,4 +8,43 @@ $(document).ready(function(){
         $(this).toggleClass("collapse").next().slideToggle();
     });
 
+    $('.category-filter__more-btn').click(function () {
+        $(this).parent('.category-filter__item').toggleClass('active');
+        if ($(this).parent('.category-filter__item').hasClass('active')) {
+            $(this).parent('.category-filter__item').find('.hidden').addClass('visible');
+            $(this).text('Cкрыть');          
+            } else {                  
+                $(this).parent('.category-filter__item').find('.hidden').removeClass('visible');
+                $(this).text($(this).data('text'));
+            }
+    });
+
+    $('.filter-btn-js').click(function () {
+		$('.category-filter').addClass('active');
+		$('body').addClass('fixed-mb');
+	});
+	$('.category-filter__close-btn--js').click(function () {
+		$('.category-filter').removeClass('active');
+		$('body').removeClass('fixed-mb');
+	});
+	$('.category-filter__submit').click(function () {
+		$('.category-filter').removeClass('active');
+		$('body').removeClass('fixed-mb');
+	});
+    
+
+    // Tabs
+	$('.tabs__caption').on('click', '.tabs__btn:not(.active)', function (e) {
+		$(this)
+			.addClass('active').siblings().removeClass('active')
+			.closest('.tabs').find('.tabs__content').hide().removeClass('active')
+			.eq($(this).index()).fadeIn(50).addClass('active');
+	});
+
+
+    $('.line-filter__sort-btn').click(function () {
+        $('.line-filter__sort').slideToggle();
+    });
+
+
 })
