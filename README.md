95b6efb3733dd92a766db76ba221860cb32eb4c4
EkaterinaKochneva <kochnevaes@gmail.com>
2023-02-28 15:54:32 +0300

    some new pages

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 11. https://ekaterinakochneva.github.io/frear/contact.html
 12. https://ekaterinakochneva.github.io/frear/about.html
 13. https://ekaterinakochneva.github.io/frear/vypolnennye-raboty.html
-14. https://ekaterinakochneva.github.io/frear/otoplenie.html
+14. https://ekaterinakochneva.github.io/frear/ustanovka-radiatorov-otopleniya.html
 15. https://ekaterinakochneva.github.io/frear/ochistka-vody.html
 16. https://ekaterinakochneva.github.io/frear/burenie-skvazhin-na-vodu.html 
 
@@ -23,3 +23,5 @@
 20. https://ekaterinakochneva.github.io/frear/razvodka-trub-vodosnabzheniya.html
 21. https://ekaterinakochneva.github.io/frear/stati.html
 22. https://ekaterinakochneva.github.io/frear/chistka-septika.html
+23. https://ekaterinakochneva.github.io/frear/otoplenie-html.html
+24. https://ekaterinakochneva.github.io/frear/elektricheskoe-otoplenie-v-chastnom-dome.html
--- docs/js/page.js
+++ docs/js/page.js
@@ -30,11 +30,36 @@ $(document).ready(function(){
 		  },
 	});
 
+	// Cлайдер выполненных работ
+	const swiperСompleted = new Swiper('.completed-gallery__slider', {
+		loop: true,
+		slidesPerView: 1,
+		draggable: true,
+		autoplay: {
+			delay: 5000,
+		  },
+
+        navigation: {
+        nextEl: '.completed-gallery__button-next',
+        prevEl: '.completed-gallery__button-prev',
+        },
+		pagination: {
+			el: '.completed-gallery__pagination',
+			clickable: true,
+		  },
+	});
+
 	// Плавная прокрутка
 	$(".smooth-scroll-link-js").on("click", function (e) {
 		e.preventDefault();
 		var get_id = $(this).attr('href');
 		var target = $(get_id).offset().top; 
 		$("html, body").animate({ scrollTop: target }, 500);
+	});
+
+	// Аккардион FAQ
+	$('.faq__btn').click(function(){
+		$(this).toggleClass('active');
+		$(this).parent().next().slideToggle();
 	});
 })
